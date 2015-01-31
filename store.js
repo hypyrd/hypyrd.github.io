e07bd15766801b452f44d0a9183585153196724c
raorao <savinirs@gmail.com>
2015-01-31 13:38:16 -0800

    removes javascript for start of tutorial

--- store.js
+++ store.js
@@ -16,44 +16,7 @@ ListStore = {
     return items
   },
 
-  loadItems: function() {
-    var loadRequest = $.ajax({
-      type: 'GET',
-      url: "https://listalous.herokuapp.com/lists/YOUR-LIST-NAME-HERE/"
-    })
-
-    loadRequest.done(function(dataFromServer) {
-      items = dataFromServer.items
-      notifyComponents()
-    })
-  },
-
-  addItem: function(itemDescription) {
-    var creationRequest = $.ajax({
-      type: 'POST',
-      url: "http://listalous.herokuapp.com/lists/YOUR-LIST-NAME-HERE/items",
-      data: { description: itemDescription, completed: false }
-    })
-
-    creationRequest.done(function(itemDataFromServer) {
-      items.push(itemDataFromServer)
-      notifyComponents()
-    })
-  },
-
-  toggleCompleteness: function(itemId) {
-    var item = findItemById(itemId)
-    var currentCompletedValue = item.completed
-
-    var updateRequest = $.ajax({
-      type: 'PUT',
-      url: "https://listalous.herokuapp.com/lists/YOUR-LIST-NAME-HERE/items/" + itemId,
-      data: { completed: !currentCompletedValue }
-    })
-
-    updateRequest.done(function(itemData) {
-      item.completed = itemData.completed
-      notifyComponents()
-    })
-  }
+  loadItems: function() {},
+  addItem: function(itemDescription) {},
+  toggleCompleteness: function(itemId) {}
 }
